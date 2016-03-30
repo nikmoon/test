395b2c56787fb0832ba6e58b5ad642aa8262eafc
unknown <consensuss@rambler.ru>
2016-03-30 12:32:21 +0700

    Added lobbyapp.urls and created lobbyapp templates dir

--- lobby/urls.py
+++ lobby/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lobbyapp import views
+import lobbyapp.urls
 
 urlpatterns = [
+    url(r'^lobby/', include(lobbyapp.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^lobby/', views.index),
 ]
--- lobbyapp/views.py
+++ lobbyapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+
+from lobby import settings
 
 # Create your views here.
-
+#@login_required
+def index(request):
+    return HttpResponse('Hi человеки')
