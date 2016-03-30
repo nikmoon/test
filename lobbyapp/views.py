28cfe8d5d65beeb7843d4700de0fc885c9b348fe
nikmoon <consensuss@rambler.ru>
2016-03-30 01:31:35 +0000

    First commit

---
+++ lobbyapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+# Create your views here.
+
