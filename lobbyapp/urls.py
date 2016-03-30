79c2f13c5304d668f024a24e490cc33d9e9a4573
unknown <consensuss@rambler.ru>
2016-03-30 16:55:26 +0700

    Added some registration

--- lobbyapp/urls.py
+++ lobbyapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+#    url(r'^registration/$', views.registration),
+    url(r'^registration/$', views.RegisterFormView.as_view()),
+    url(r'^login/$', views.login),
+    url(r'^logout/$', views.logout),
 ]
--- lobbyapp/views.py
+++ lobbyapp/views.py
@@ -4,9 +4,45 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 
-from lobby import settings
 
 # Create your views here.
-#@login_required
 def index(request):
-    return HttpResponse('Hi человеки')
+    #user = authenticate(username='vasyan', password='director123')
+    #print(type(user))
+    users = [user.username for user in User.objects.all()]
+    return HttpResponse('Hi человеки. ' + str(users))
+
+
+def registration(request):
+    return HttpResponse('Здесь у нас будет страничка регистрации. Загляните позже...')
+
+
+def login(request):
+    return HttpResponse('Здесь у нас будет страничка входа. Загляните позже...')
+
+
+def logout(request):
+    return HttpResponse('Сюда отправляются пользователи после выхода. Загляните позже...')
+
+
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
+    success_url = "../login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "lobbyapp/registration.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя, если данные в форму были введены корректно.
+        form.save()
+
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+
